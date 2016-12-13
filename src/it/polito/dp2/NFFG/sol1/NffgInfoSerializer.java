70380f75d6b7ba9fb4a17f2acd7b65c41c4ec0fd
Giacomo <jgiak92@gmail.com>
2016-12-13 22:33:16 +0100

    new xsd + draft serializer

--- src/it/polito/dp2/NFFG/sol1/NffgInfoSerializer.java
+++ src/it/polito/dp2/NFFG/sol1/NffgInfoSerializer.java
@@ -1,14 +1,32 @@
 package it.polito.dp2.NFFG.sol1;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+import it.polito.dp2.NFFG.NffgReader;
 import it.polito.dp2.NFFG.NffgVerifier;
 import it.polito.dp2.NFFG.NffgVerifierException;
 import it.polito.dp2.NFFG.NffgVerifierFactory;
+import it.polito.dp2.NFFG.sol1.jaxb.NffgType;
+import it.polito.dp2.NFFG.sol1.jaxb.NffgVerifierType;
+import it.polito.dp2.NFFG.sol1.jaxb.ObjectFactory;
+
 public class NffgInfoSerializer {
 
 	private NffgVerifier verifier;
+	private NffgVerifierType jaxbVerifier;
+	private ObjectFactory objFact;
 
 	/**
 	 * Default constructror
+	 *
 	 * @throws NffgVerifierException
 	 */
 	public NffgInfoSerializer() throws NffgVerifierException {
@@ -18,13 +36,16 @@ public class NffgInfoSerializer {
 
 	/**
 	 * Constructor with verifier
+	 *
 	 * @param verifier
 	 */
 	public NffgInfoSerializer(NffgVerifier verifier) {
 		super();
-		if (verifier != null)
+		if (verifier != null) {
 			this.verifier = verifier;
-		else
+			this.objFact = new ObjectFactory();
+			this.jaxbVerifier = objFact.createNffgVerifierType();
+		} else
 			throw new IllegalArgumentException("Missing Verifier");
 	}
 
@@ -40,11 +61,44 @@ public class NffgInfoSerializer {
 			System.err.println("Could not instantiate data generator.");
 			e.printStackTrace();
 			System.exit(1);
+		} catch (JAXBException e) {
+			System.err.println("Could not instantiate the JAXB context.");
+			e.printStackTrace();
+			System.exit(1);
+		} catch (FileNotFoundException e) {
+			System.err.println("Could not find the file.");
+			e.printStackTrace();
+			System.exit(1);
 		}
 	}
-	
-	public void exportToXML(String outputFile){
-		
+
+	public void exportToXML(String outputFile) throws JAXBException, FileNotFoundException {
+		JAXBContext context = JAXBContext.newInstance("it.polito.dp2.NFFG.sol1.jaxb");
+		Marshaller marsh = context.createMarshaller();
+
+		copyNffgs();
+
+		JAXBElement<NffgVerifierType> jaxbElement = objFact.createNffgVerifier(jaxbVerifier);
+
+
+		FileOutputStream output = new FileOutputStream(outputFile);
+		marsh.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT,
+				Boolean.TRUE);
+		marsh.marshal(jaxbElement, output);
+
+	}
+
+	private void copyNffgs() {
+		NffgVerifierType.Nffgs nffgs = new NffgVerifierType.Nffgs();
+
+		List<NffgType> nffgList = nffgs.getNffg();
+		for(NffgReader graph : verifier.getNffgs()){
+			NffgType nffg = new NffgType();
+
+			nffgList.add(nffg);
+		}
+		jaxbVerifier.setNffgs(nffgs);
+
 	}
 
 }
